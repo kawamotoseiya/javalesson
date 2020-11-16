96afb949191caca487762a1074cb0c0942e7b4c1
kawamotoseiya <seinnto1226@icloud.com>
2020-11-17 02:51:09 +0900

    lesson15-2

--- practice2/resouces/Chapter15_2.java
+++ practice2/resouces/Chapter15_2.java
@@ -18,16 +18,24 @@ public class Chapter15_2 {
         // Java7以降に追加された「try-with-resources」文で処理
         // Filesクラスを使用して、BufferedReaderクラスのインスタンスを生成
         // Filesクラスを使用して、BufferedWriterクラスのインスタンスを生成
-        try () {
-
+        try (BufferedReader reader = Files.newBufferedReader(inputPath);
+        		BufferedWriter writer = Files.newBufferedWriter(outputPath);) {
             String line = null;
             // BufferedReader.readLineメソッドを使用して、1行づつ処理
             // 読み込む行がなくなるまでループ
-            while () {}
+            while ((line = reader.readLine()) != null) {
+            	 if (line.length() != 5) {
+                     continue;
+                 }
+            	 writer.write(line);
+            	 writer.newLine();
+            }
 
         // 例外処理を記述
         // printStackTraceで例外を出力
-        } catch ()  {} finally {
+        } catch (IOException e)  {
+        	 e.printStackTrace();
+        } finally {
             System.out.println("出力処理終了");
         }
     }
